36010d5c077bc1560c7f6d14cec06585faf052c1
Zhangzhenguo3352 <13012113210@163.com>
2016-08-18 18:17:50 +0800

    首页样式 架构完成

--- src/Component/IndexList.js
+++ src/Component/IndexList.js
@@ -9,43 +9,39 @@ export class Footer extends Component {
     return(
       <div >
       <ul className="display-flex Footer-content">
-        <li className="flex-1 active">
-          精华2
-
+        <li className="flex-1">
+          地区
+          <i className="iconfont f_s_10 pa_L_5px">&#xe61d;</i>
         </li>
         <li className="flex-2 se_bj_c_01 se_fn_c">
             <form className="bag_box">
-                <div></div>
+                <div><i className="iconfont">&#xe616;</i></div>
                 <input type="text" placeholder="数商家、分类或商家" className=""/>
             </form>
         </li>
         <li className="flex-1">
-          精华1
+          <i className="iconfont">&#xe607;</i>
 
         </li>
         <li className="flex-1">
 
-          精华1
+          <i className="iconfont">&#xe618;</i>
         </li>
         
       </ul></div>
       )
     }
 }
+
+
 export class DataLoad extends Component {
-    render() {
-        let {loadAnimation, loadMsg} = this.props;
-        return (
-            <div className={'data-load data-load-' + loadAnimation}>
-                <div className="msg">{loadMsg}</div>
-            </div>
-        );
-    }
+  render(){
+    return(
+      <div>123</div>
+      )
+  }
 }
-DataLoad.defaultProps = {
-    loadAnimation: true, //默认显示加载动画
-    loadMsg: '正在加载中'
-}
+
 export class Nav extends Component {
 
 
@@ -54,19 +50,28 @@ export class Nav extends Component {
         <ul className="Header-box display-flex">
           <li className="flex-1">
               <Link to="/Name1" activeClassName="active" >
-              <i className="iconfont">&#xe607;</i>
-              首页
+              <i className="iconfont">&#xe612;</i>
+              <div>首页</div>
               </Link>
           </li>
           <li className="flex-1">
-              <Link to="/Name2" activeClassName="active">商家</Link>
+              <Link to="/Name2" activeClassName="active">
+              <i className="iconfont">&#xe619;</i>
+              <div>商家</div>
+              </Link>
           </li>
           <li className="flex-1">
 
-              <Link to="/Name3" activeClassName="active">我们</Link>
+              <Link to="/Name3" activeClassName="active">
+              <i className="iconfont">&#xe600;</i>
+              <div>我的</div>
+              </Link>
           </li>
           <li className="flex-1">
-              <Link to="/Name4" activeClassName="active">更多</Link>
+              <Link to="/Name4" activeClassName="active">
+              <i className="iconfont">&#xe603;</i>
+              <div>更多</div>
+              </Link>
           </li>
         </ul>
       )
--- src/Component/common/index.js
+++ src/Component/common/index.js
@@ -1,26 +1,94 @@
 import React, {Component} from 'react';
+
+
 import {Nav, Footer,DataLoad} from '../indexList.js';
 
-
-
+{/*首页*/}
 export class Name1 extends Component{
     render(){
         return(
             <div>
                 <Nav></Nav>
-                name1
+                <div className="content_mar_T_40">
+                    <div className="home_content">轮播图</div>
+                <div className="pa_top_10px bj_fff display-flex H-80">
+                    <div className="flex-1 bor_R_bor_c">111</div>
+                    <div className="flex-1 display-flex flex-column">
+                        <div className="flex-1">1111</div>
+                        <div className="flex-1 bor_T_bor_c">1111</div>
+                    </div>
+                </div>
+                <div className="bj_fff pa_top_10px">
+                    <div className="display-flex H-50 ">123</div>
+                    <div className="display-flex">
+                        <div className="flex-1 bor_R_bor_c display-flex flex-column">
+                            <div className="flex-1 H-40 bor_T_bor_c">111111</div>
+                            <div className="flex-1 H-40 bor_T_bor_c">111111</div>
+                            
+                        </div>
+                        <div className="flex-1 display-flex flex-column">
+                            <div className="flex-1 H-40 bor_T_bor_c">222222</div>
+                            <div className="flex-1 H-40 bor_T_bor_c">222222</div>
+                            
+                        </div>
+                    </div>
+                </div>
+
+                {/*猜你喜欢*/}
+                <div className="bj_fff pa_top_10px">
+                    <div className="f_cor_LuSe f_s_10 padd_TRBL_10">
+                        <i className="iconfont f_s_10">&#xe605;</i>
+                        <span className="ma_L_5px">猜你喜欢</span>
+                    </div>
+                    <div className="display-flex H-80 bor_T_bor_c">
+                        <div className="W-60 H-60 ser_mar_self">123</div>
+                        <div className="flex-1 padd_TRBL_10">
+                            <h3 className="f_s_10">一元夺iPhone6</h3>
+                            <p className="fn_we_normal f_s_10">[全国]超高中奖率，不信来试！</p>
+                        </div>
+                    </div>
+                    <div className="display-flex H-80 bor_T_bor_c">
+                        <div className="W-60 H-60 ser_mar_self">123</div>
+                        <div className="flex-1 padd_TRBL_10">
+                            <h3 className="f_s_10">一元夺iPhone6</h3>
+                            <p className="fn_we_normal f_s_10">[全国]超高中奖率，不信来试！</p>
+                        </div>
+                    </div>
+                    <div className="display-flex H-80 bor_T_bor_c">
+                        <div className="W-60 H-60 ser_mar_self">123</div>
+                        <div className="flex-1 padd_TRBL_10">
+                            <h3 className="f_s_10">一元夺iPhone6</h3>
+                            <p className="fn_we_normal f_s_10">[全国]超高中奖率，不信来试！</p>
+                        </div>
+                    </div>
+                    <div className="display-flex H-80 bor_T_bor_c">
+                        <div className="W-60 H-60 ser_mar_self">123</div>
+                        <div className="flex-1 padd_TRBL_10">
+                            <h3 className="f_s_10">一元夺iPhone6</h3>
+                            <p className="fn_we_normal f_s_10">[全国]超高中奖率，不信来试！</p>
+                        </div>
+                    </div>
+                </div>
+
+                </div>
+                
+                
+
                 <Footer/>
             </div>
             )
     }
 }
 
+{/*商家*/}
 export class Name2 extends Component{
     render(){
         return(
             <div>
                 <Nav></Nav>
-                name2
+                <div className="content_mar_T_40">
+wew
+                </div>
                 <Footer/>
             </div>
             )
@@ -32,7 +100,13 @@ export class Name3 extends Component{
         return(
             <div>
                 <Nav></Nav>
-                name3
+                <div>name3</div>
+                <div>name3</div>
+                <div>name3</div>
+                <div>name3</div>
+                <div>name3</div>
+                <div>name3</div>
+                <div>name3</div>
                 <Footer/>
             </div>
             )
@@ -56,7 +130,7 @@ export class Name5 extends Component{
         return(
             <div>
                 <Nav></Nav>
-                name5
+                
                 <Footer/>
             </div>
             )
